ac7517926dbc0795c501a459b1f3f9d3a1f056d5
SyncfusionBuild <buildautomation@syncfusion.com>
2021-11-12 16:03:57 +0530

    v19.3.0.53

---
+++ radialgauge/Views/Axis/CustomLabels.xaml.cs
@@ -0,0 +1,75 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace syncfusion.radialgaugedemos.winui.Views
+{
+    using Microsoft.UI.Xaml.Controls;
+    using Syncfusion.UI.Xaml.Gauges;
+    using System;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CustomLabels : Page, IDisposable
+    {
+        public CustomLabels()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RadialAxis_LabelPrepared(object sender, LabelPreparedEventArgs e)
+        {
+            if (e.LabelText == "80" || e.LabelText == "0")
+            {
+                e.LabelText = "N";
+            }
+            else if (e.LabelText == "10")
+            {
+                e.LabelText = "NE";
+            }
+            else if (e.LabelText == "20")
+            {
+                e.LabelText = "E";
+            }
+            else if (e.LabelText == "30")
+            {
+                e.LabelText = "SE";
+            }
+            else if (e.LabelText == "40")
+            {
+                e.LabelText = "S";
+            }
+            else if (e.LabelText == "50")
+            {
+                e.LabelText = "SW";
+            }
+            else if (e.LabelText == "60")
+            {
+                e.LabelText = "W";
+            }
+            else if (e.LabelText == "70")
+            {
+                e.LabelText = "NW";
+            }
+        }
+
+        /// <summary>
+        /// To dispose objects.
+        /// </summary>
+        public void Dispose()
+        {
+            this.radialAxis.LabelPrepared -= RadialAxis_LabelPrepared;
+#if WinUI_Desktop
+            this.gauge.Dispose();
+#endif
+        }
+    }
+}
---
+++ scheduler/Views/TimelineResourceGrouping.xaml.cs
@@ -0,0 +1,58 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace syncfusion.schedulerdemos.winui
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class TimelineResourceGrouping : Page, IDisposable
+	{
+		public TimelineResourceGrouping()
+		{
+			this.InitializeComponent();
+			this.scheduler.DaysViewSettings.MinimumAppointmentDuration = new TimeSpan(0, 30, 0);
+			this.scheduler.TimelineViewSettings.MinimumAppointmentDuration = new TimeSpan(0, 30, 0);
+		}
+
+		/// <summary>
+		/// Dispose all the allocated resources.
+		/// </summary>
+		public void Dispose()
+		{
+			if (this.scheduler != null)
+			{
+				this.scheduler.Dispose();
+				this.scheduler = null;
+			}
+
+			if (this.DataContext is SchedulerBindingViewModel)
+			{
+				(this.DataContext as SchedulerBindingViewModel).Dispose();
+				this.DataContext = null;
+			}
+		}
+	}
+}
