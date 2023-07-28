4b4826628e01ec88623fe07ce01579263c216381
SyncfusionBuild <buildautomation@syncfusion.com>
2023-07-28 04:47:02 +0000

    v22.2.5

--- calendar/Properties/AssemblyInfo.cs
+++ calendar/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.1.34")]
+[assembly: AssemblyVersion("22.2.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
